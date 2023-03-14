ae96b270c2e3203a22b3e05d70076992612e4568
artikareliya <126856555+artikareliya@users.noreply.github.com>
2023-03-14 13:01:05 +0530

    Commit "EditPage"

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -27,5 +27,40 @@ namespace BlogTutorial.Controllers
             db.SaveChanges();
             return View();
         }
+
+        //Display data on EditPost page
+        public IActionResult EditPost()
+        {
+            var myEditPost = db.tblPost;
+            return View(myEditPost);
+        }
+
+        //Delete Post from Database on EditPost page
+        public IActionResult DeletePost(int id)
+        {
+            var DeletePost = db.tblPost.Find(id);
+            if (DeletePost!=null)
+            {           
+                db.tblPost.Remove(DeletePost);
+                db.SaveChanges();
+            }
+            return RedirectToAction("EditPost", "Admin");
+        }
+
+        //Update Action and redirect on UpdatePage with selected data by Id
+        public IActionResult UpdatePost(int id)
+        {
+            var postupdate = db.tblPost.Find(id);
+           return View(postupdate);
+        }
+
+        [HttpPost]
+        //Update data in database on update page
+        public IActionResult UpdatePost(Post post)
+        {
+            db.tblPost.Update(post);
+            db.SaveChanges();
+            return RedirectToAction("EditPost", "Admin");
+        }
     }
 }
